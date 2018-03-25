a3d516663ee25114b09700b1f968a09074e804de
Victor Titor <vtitor.edumix@gmail.com>
2018-03-25 21:18:07 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='caller',
+    version='0.0.1',
+    author='Victor Titor',
+    author_email='vtitor.edumix@gmail.com',
+    url='https://github.com/vtitor/caller',
+    py_modules=['caller'],
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.6'
+    ],
+)
